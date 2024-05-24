8bc5064c325eab98e8c2381a168efb24e721eebb
Colemanhawkins <gabrielcaba@live.com>
2024-05-24 15:18:04 -0300

    challenge-02

--- Exercises/challenge-01/challenge-01.ts
+++ Exercises/challenge-01/challenge-01.ts
@@ -1,16 +1,15 @@
 function wrapping(gifts: string[]): string[] {
 
-  let wrappedGifts: string[] = []
+  let wrappedGifts: string[] = [];
   
-  gifts.forEach((gift) => {
-    const papper = '*'
-    const box = papper.repeat( 2 + gift.length)
-    const wrapper = box + `\n*${gift}*\n` +  box;
-    wrappedGifts.push(wrapper)
-  
-  })
-  console.log(wrappedGifts)
-      
-  return  wrappedGifts
-   }
+    gifts.forEach((gift) => {
+      const papper = '*';
+      const box = papper.repeat( 2 + gift.length);
+      const wrapper = box + `\n*${gift}*\n` +  box;
+      wrappedGifts.push(wrapper);
+    
+    })
+ 
+  return  wrappedGifts;
+}
     
