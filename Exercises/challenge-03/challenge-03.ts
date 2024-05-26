d6a2aea5f3227bb35fd08d2149df793adf2d9520
Colemanhawkins <gabrielcaba@live.com>
2024-05-25 23:43:58 -0300

    challenge-03

---
+++ Exercises/challenge-03/challenge-03.ts
@@ -0,0 +1,12 @@
+function distributeGifts(packOfGifts: string[],reindeers: string[]): number {
+    const giftsWeidht = packOfGifts.join('').length
+    const reindeersCapacityWeidth = reindeers.join('').length * 2
+    giftsWeidht < reindeersCapacityWeidth ?
+    Math.floor(reindeersCapacityWeidth / giftsWeidht)
+    : 0
+}
+
+const packOfGifts = ['regalo1', 'regalo2'] //esto daria un peso de 14
+const reindeers = ['rudolf']
+
+distributeGifts(packOfGifts,reindeers)
